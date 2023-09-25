4c2543a65bb496a800bed9fe4c59e77186f39ba9
lizhikui <lizhikui@xiaoniangao.com>
2023-09-25 08:57:13 +0800

    测试close channel的时候会唤醒所有的接受者

--- lesson8/main_test.go
+++ lesson8/main_test.go
@@ -57,3 +57,31 @@ func TestType(t *testing.T) {
 	tm := reflect.TypeOf(map[string]string{})
 	fmt.Println(tm, tm.Name())
 }
+
+func ld(c1 chan struct{}, index int) {
+	for {
+		_, ok := <-c1
+		if ok {
+			fmt.Println(index, "ok")
+			break
+		} else {
+			fmt.Println(index, "wait")
+			time.Sleep(time.Second)
+		}
+	}
+}
+
+func TestContext1(t *testing.T) {
+	c1 := make(chan struct{}, 1)
+
+	go ld(c1, 1)
+	go ld(c1, 2)
+
+	time.Sleep(2 * time.Second)
+
+	c1 <- struct{}{} //发送消息的时候，只有一个协程会处理消息
+	//close(c1) //close的时候，所有的协程都会被唤醒处理，只是接收不到可处理的消息，context就是利用了这个特性
+
+	time.Sleep(time.Hour)
+
+}
